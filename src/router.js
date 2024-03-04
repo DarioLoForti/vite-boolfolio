9e5af5d2c3e54315dec988040c262cfbef77ae2b
DarioLoForti <lofortidario@gmail.com>
2024-03-04 16:43:31 +0100

    BONUS - creo la pagina AppNotFound per gestire gli errori di URL

--- src/router.js
+++ src/router.js
@@ -4,6 +4,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import AppProjects from './pages/AppProjects.vue';
 
+import AppNotFound from './pages/AppNotFound.vue';
+
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -16,6 +19,11 @@ const router = createRouter({
             path: '/projects',
             name: 'projects',
             component: AppProjects
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'not-found',
+            component: AppNotFound
         }
     ]
 });
