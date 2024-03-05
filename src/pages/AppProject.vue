0463aedaf5e7fe351081a4b56132ff604acc2581
DarioLoForti <lofortidario@gmail.com>
2024-03-05 15:59:48 +0100

    creo pagina di visualizzazione progetto singolo e richiamo il metodo show per visualizzare un solo progetto

--- src/pages/AppProject.vue
+++ src/pages/AppProject.vue
@@ -1,13 +1,53 @@
 <script>
+import { store } from '../store.js'
+import axios from 'axios';
+
 export default {
-    
+    name: 'AppProject',
+    data(){
+        return{
+            store,
+            project: null,
+        }
+    },
+    created(){
+        this.getProject();
+    },
+    methods: {
+        getProject() {
+            axios.get(`${this.store.baseUrl}/api/projects/${this.$route.params.slug}`,).then((response) => {
+                this.project = response.data.project;
+            });
+        }
+    }
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div class="container">
+        <div class="roe">
+            <div class="col-12">
+                <div class="image">
+                    <img :src="`${this.store.baseUrl}/storage/${project.cover_image}`" alt="project.titolo">
+                </div>
+                <div class="col-12">
+                    <h1 class="text-center py-5">Titolo: {{ project.titolo}} </h1>
+                </div>
+                <div class="col-12">
+                    <p> Descrizione: <br> <br>
+                        {{project.descrizione}}</p>
+                </div>
+                <div class="col-12">
+                    <p> Autore:  {{project.autore}} </p>
+                </div>
+                <div class="col-12">
+                    <p> Data fine progetto: {{project.fine_progetto}} </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-<style lang="">
+<style lang="scss" scoped>
+@use '../styles/generals.scss' as*;
+
     
 </style>
